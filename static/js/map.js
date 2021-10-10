8db2b0664a689ba2308c822549c0a1c516a68393
Litesh Samji <litesh@y7mail.com>
2021-10-10 15:44:56 +1100

    top 10 cyclone map object

---
+++ static/js/map.js
@@ -0,0 +1,16 @@
+// Create a map object
+var myMap = L.map("map-id", {
+    center: [-27.546690, 135.446730],
+    zoom: 5
+  });
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(myMap);
+
+let url = ("/static/js/cyclones_top10.json");
